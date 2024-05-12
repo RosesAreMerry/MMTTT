e2ffa9b36daa0b390a01dd55c5b6262afa4ad3ef
Rosemary Roach <rosemary@dopeapps.com>
2024-05-12 15:02:18 -0700

    Rewrote selectMove and miniMax

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -1,80 +1,49 @@
 // Tic Tac Toe
 class Agent {
-    constructor() {
+    constructor() { }
 
-    }
-
-    minimax(board, isMaximizing) {
-        // Base cases - check if the game is over or a draw
-        var gameOver = board.gameOver();
-        if (gameOver === 1) {
-            return 1; // X wins
-        } else if (gameOver === 2) {
-            return -1; // O wins
-        } else if (gameOver === 3) {
-            return 0; // the game is a draw
-        }
-
-        // Recursive case - evaluate all possible moves and choose the best score
-        if (isMaximizing) {
-            var bestScore = -Infinity;
-            for (var i = 0; i < board.cells.length; i++) {
-                var cell = i + 1;
-                if (board.cellFree(cell)) {
-                    var newBoard = board.clone();
-                    newBoard.move(cell);
-                    var score = this.minimax(newBoard, false);
-                    bestScore = Math.max(bestScore, score);
-                }
-            }
-            return bestScore;
-        } else {
-            var bestScore = Infinity;
-            for (var i = 0; i < board.cells.length; i++) {
-                var cell = i + 1;
-                if (board.cellFree(cell)) {
-                    var newBoard = board.clone();
-                    newBoard.move(cell);
-                    var score = this.minimax(newBoard, true);
-                    bestScore = Math.min(bestScore, score);
-                }
-            }
-            return bestScore;
-        }
-    }
 
     selectMove(board) {
-        // Define the initial best score and move
-        var maxScore = -Infinity;
-        var maxMove = null;
-
-        var minScore = Infinity;
-        var minMove = null;
-
-        // Loop through each cell to evaluate the best move
-        for (var i = 0; i < board.cells.length; i++) {
-            var cell = i + 1;
-            if (board.cellFree(cell)) {
-                // Make a move on the current cell
-                var newBoard = board.clone();
-                newBoard.move(cell);
-
-                // Calculate the score for the current move
-                var score = this.minimax(newBoard, !board.playerOne);
-
-                // Update the best move if the current move has a higher score
-                if (score > maxScore) {
-                    maxScore = score;
-                    maxMove = cell;
-                }
-                if (score < minScore) {
-                    minScore = score;
-                    minMove = cell;
-                }
+        const maxing = this.xTurn(board);
+        const empty = this.emptyCells(board)
+        let move = -1;
+        let moveScore = maxing ? -Infinity : Infinity;
+        for (const emptyCell of empty) {
+            const newScore = this.minimax(board, emptyCell) 
+            if (maxing && newScore > moveScore) {
+                move = emptyCell;
+                moveScore = newScore;
+            }
+            if (!maxing && newScore < moveScore) {
+                move = emptyCell;
+                moveScore = newScore;
             }
         }
+        return move;
+    }
 
-        return board.playerOne ? maxMove : minMove;
+    minimax(oldBoard, move) {
+        const board = oldBoard.clone();
+        board.move(move);
+
+        // Base case
+        switch (board.gameOver()) {
+            case 1: return 1;
+            case 2: return -1;
+            case 3: return 0;
+        }
+
+        const scores = this.emptyCells(board).map((emptyCell) => this.minimax(board, emptyCell));
+
+        return this.xTurn(board) ? Math.max(...scores) : Math.min(...scores);
+    }
+
+    xTurn(board) {
+        return board.X.length == board.O.length;
+    }
+
+    emptyCells(board) {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9].filter((cell) => board.cellFree(cell));
     }
 
 }
